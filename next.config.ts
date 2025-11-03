c3afc28788b693d07ab54cff6fd8eee289f3b8cf
Alan-Kim <r0949915@ucll.be>
2025-11-03 10:59:58 +0100

    pray

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new (require("@module-federation/nextjs-mf").ModuleFederationPlugin)({
+        name: "dummytool1",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {},
+        shared: {
+          react: { singleton: true, eager: true, requiredVersion: false },
+          "react-dom": { singleton: true, eager: true, requiredVersion: false },
+        },
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
